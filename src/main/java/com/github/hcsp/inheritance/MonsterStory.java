8bdb9b125ff32bdeef1ffe4bbf04885f3da4b2fd
Lingfeng <2354833@qq.com>
2019-06-29 17:14:15 +0800

    老和尚 变成 老妖怪

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -8,8 +8,13 @@ public class MonsterStory extends Story {
     // 故事讲完啦
     // 你还想听吗
     @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+
+    @Override
     public void endStory() {
-        System.out.println("故事讲完啦");
+        super.endStory();
         System.out.println("你还想听吗");
     }
 
