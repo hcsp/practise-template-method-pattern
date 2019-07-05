ff3995a59b8dd9712593a2bf1a1af5760506904c
slTrust <25893245+slTrust@users.noreply.github.com>
2019-07-05 14:55:55 +0800

    template (#21)
    template (#21)

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -7,6 +7,21 @@ public class MonsterStory extends Story {
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    @Override
+    public void startStory() {
+        System.out.println("开始讲故事啦 ");
+    }
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪 ");
+    }
+
+    @Override
+    public void endStory(){
+        System.out.print("故事讲完啦  ");
+        System.out.println("你还想听吗");
+        super.endStory();
+    }
     public static void main(String[] args) {
         new MonsterStory().tellStory();
     }
