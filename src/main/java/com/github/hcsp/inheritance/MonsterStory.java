f5a250479f4f5ebd446dd8d9a3afe85ea8f6c2e0
uuflyer <1315684099@qq.com>
2019-08-10 17:58:10 +0800

    practise-template

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -1,13 +1,28 @@
 package com.github.hcsp.inheritance;
 
 public class MonsterStory extends Story {
+    @Override
+
+    public int hashCode() {
+        return super.hashCode();
+    }
     // 请补全本类，使得main方法可以输出以下内容：
     //
     // 开始讲故事啦
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    @Override
+    public void endStory(){
+        super.endStory();
+        System.out.println("你还想听吗");
+    }
+
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");}
     public static void main(String[] args) {
         new MonsterStory().tellStory();
     }
+
 }
