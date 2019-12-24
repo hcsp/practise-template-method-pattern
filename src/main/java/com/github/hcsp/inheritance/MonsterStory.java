4870850b1529d900b6057bf71b2b6bf7a8ddb301
Jane Wang <janewangcoa@gmail.com>
2019-12-24 19:09:18 +0800

    模板方法

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -8,6 +8,10 @@ public class MonsterStory extends Story {
     // 故事讲完啦
     // 你还想听吗
     @Override
+    public void story(){
+        System.out.println("从前有个老妖怪");
+    }
+    @Override
     public void endStory() {
         super.endStory();
         System.out.println("你还想听吗");
