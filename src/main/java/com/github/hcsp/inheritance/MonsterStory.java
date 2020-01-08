bfa9abbc5abc8540e392fc0fcaf0d2c859bef6df
spclock <1124516293@qq.com>
2020-01-08 09:36:33 +0800

    模版方法实战

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -7,7 +7,26 @@ public class MonsterStory extends Story {
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    @Override
+    public void startStory() {
+        System.out.print("开始讲故事啦");
+    }
+
+    @Override
+    public void story() {
+        System.out.print("从前有个老妖怪");
+    }
+
+    @Override
+    public void endStory() {
+        System.out.print("故事讲完啦");
+    }
+
+    public void continueStory() { System.out.print("你还想听吗"); }
+
     public static void main(String[] args) {
-        new MonsterStory().tellStory();
+        MonsterStory monsterStory = new MonsterStory();
+        monsterStory.tellStory();
+        monsterStory.continueStory();
     }
 }
