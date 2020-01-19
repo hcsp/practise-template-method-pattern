78d8b321662f430673b937cfcafd004091f25378
王文哲 <mysteryven@gmail.com>
2020-01-19 13:37:20 +0800

    使用模板方法

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -1,13 +1,20 @@
 package com.github.hcsp.inheritance;
 
-public class MonsterStory extends Story {
+public class MonsterStory  {
     // 请补全本类，使得main方法可以输出以下内容：
     //
     // 开始讲故事啦
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    Story story = new Story();
+
     public static void main(String[] args) {
         new MonsterStory().tellStory();
+    }
+
+    public void tellStory() {
+       story.tellStory();
+       System.out.println("你还想听吗");
     }
 }
