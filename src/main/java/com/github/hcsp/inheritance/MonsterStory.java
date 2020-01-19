2820f44fcea94fcd21ac81f453e9040302452c12
王文哲 <mysteryven@gmail.com>
2020-01-19 13:48:18 +0800

    组合和模板方法弄混了，改回来了。

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -1,20 +1,27 @@
 package com.github.hcsp.inheritance;
 
-public class MonsterStory  {
+public class MonsterStory  extends Story {
     // 请补全本类，使得main方法可以输出以下内容：
     //
     // 开始讲故事啦
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
-    Story story = new Story();
+
+
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+
+    @Override
+    public void endStory() {
+        super.endStory();
+        System.out.println("你还想听吗");
+    }
 
     public static void main(String[] args) {
         new MonsterStory().tellStory();
     }
 
-    public void tellStory() {
-       story.tellStory();
-       System.out.println("你还想听吗");
-    }
 }
