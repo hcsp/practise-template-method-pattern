6008348de699d77593e95b4b794747c78d1618df
XinleZhou <2320789321>
2020-03-21 20:49:28 +0800

    模板方法3

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -16,13 +16,11 @@ public class MonsterStory extends Story {
 
     @Override
     public void endStory() {
-        System.out.println("故事讲完啦");
+        super.endStory();
         System.out.println("你还想听吗");
     }
 
     public static void main(String[] args) {
-
-
         new MonsterStory().tellStory();
     }
 
