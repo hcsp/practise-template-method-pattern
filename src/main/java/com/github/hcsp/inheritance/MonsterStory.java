5f49a8bbc6bdd7184287bd1610f79bdb171cf312
JoeyPine <fanjiasong_7@outlook.com>
2020-03-30 21:13:52 +0800

    模版方法的使用

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MonsterStory extends Story {
     // 请补全本类，使得main方法可以输出以下内容：
     //
@@ -7,6 +9,29 @@ public class MonsterStory extends Story {
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    public  void tellStory() {
+        startStory();
+        story();
+        super.endStory();
+        listen();
+    }
+
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+
+    //
+//    @Override
+//    public void endStory() {
+//        System.out.println("故事讲完啦");
+//        System.out.println("你还想听吗");
+//    }
+    private void listen() {
+        System.out.println("你还想听吗");
+    }
+
+
     public static void main(String[] args) {
         new MonsterStory().tellStory();
     }
