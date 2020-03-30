ceea60c448f321b16f34706123d9bfcb1130ae83
JoeyPine <fanjiasong_7@outlook.com>
2020-03-30 21:27:28 +0800

    删掉新加和重复的方法， 重写endStory方法

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -10,28 +10,16 @@ public class MonsterStory extends Story {
     // 故事讲完啦
     // 你还想听吗
     @Override
-    public  void tellStory() {
-        super.startStory();
-        story();
-        super.endStory();
-        listen();
-    }
-
-    @Override
     public void story() {
         System.out.println("从前有个老妖怪");
     }
 
     //
-//    @Override
-//    public void endStory() {
-//        System.out.println("故事讲完啦");
-//        System.out.println("你还想听吗");
-//    }
-    private void listen() {
+    @Override
+    public void endStory() {
+        super.endStory();
         System.out.println("你还想听吗");
     }
-
 
     public static void main(String[] args) {
         new MonsterStory().tellStory();
