4ee8b1f6a064d95e7a98802e611d4b0afc74d6b8
zhu <z823511280>
2020-09-11 19:42:31 +0800

    你還想聽嗎

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -8,6 +8,9 @@ public class MonsterStory extends Story {
     // 故事讲完啦
     // 你还想听吗
     public static void main(String[] args) {
+
         new MonsterStory().tellStory();
+       System.out.println("你还想听吗");
+
     }
 }
