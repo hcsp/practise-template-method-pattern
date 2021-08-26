676197f5ce7afa2258a69f572ddcedc5ac380e7d
“idpeng” <630606938@qq.com>
2021-08-26 10:21:22 +0800

    补全本类，使得main方法可以输出以下内容

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -7,7 +7,15 @@ public class MonsterStory extends Story {
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+    public void end() {
+        System.out.println("你还想听吗");
+    }
     public static void main(String[] args) {
         new MonsterStory().tellStory();
+        new MonsterStory().end();
     }
 }
