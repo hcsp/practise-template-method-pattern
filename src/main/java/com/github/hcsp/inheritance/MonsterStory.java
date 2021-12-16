acb2b1e3efd0e8c610a7e5f1f529fb7d7f83b920
jcbound.ma <46434117+jcbound@users.noreply.github.com>
2021-12-16 23:40:57 +0800

    模板式写法 (#244)
    模板式写法 (#244)

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -3,6 +3,16 @@ package com.github.hcsp.inheritance;
 public class MonsterStory extends Story {
     // 请补全本类，使得main方法可以输出以下内容：
     //
+    @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+    @Override
+    public void endStory() {
+        super.endStory();
+        System.out.println("你还想听吗");
+    }
+
     // 开始讲故事啦
     // 从前有个老妖怪
     // 故事讲完啦
