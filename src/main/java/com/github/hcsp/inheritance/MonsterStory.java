002d710ed0509964aeed561398e2c4f1b111a03c
Yangbisheng <2606424906@qq.com>
2022-02-01 03:04:14 +0800

    override story() method

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -8,8 +8,17 @@ public class MonsterStory extends Story {
     // 故事讲完啦
     // 你还想听吗
     @Override
+    public void startStory() {
+        System.out.print("开始讲故事啦 ");
+    }
+
+    @Override
     public void story() {
-        System.out.println("从前有个老妖怪");
+        System.out.print("从前有个老妖怪 ");
+    }
+    @Override
+    public void endStory() {
+        System.out.print("故事讲完啦 ");
     }
 
     public static void main(String[] args) {
