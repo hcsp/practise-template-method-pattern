794be288f8d2ac858a709e7a257045e2d86c8c76
DDB324 <jiang128193895>
2022-02-11 21:50:33 +0800

    模版方法 修改bug

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -12,6 +12,10 @@ public class MonsterStory extends Story {
     }
 
     @Override
+    public void story() {
+        System.out.println("从前有个老妖怪");
+    }
+
     public void endStory() {
         super.endStory();
         System.out.println("你还想听吗");
