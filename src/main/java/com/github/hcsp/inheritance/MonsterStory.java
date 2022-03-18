66c5a89e2b385ce045fa1fa912421aaec3d1c234
yezhiguang <847786582@qq.com>
2022-03-18 15:15:57 +0800

    模板方法测试

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -7,7 +7,12 @@ public class MonsterStory extends Story {
     // 从前有个老妖怪
     // 故事讲完啦
     // 你还想听吗
+    public void monsterTellStory(){
+        tellStory();
+        System.out.println("你还想听吗");
+    }
+
     public static void main(String[] args) {
-        new MonsterStory().tellStory();
+        new MonsterStory().monsterTellStory();
     }
 }
