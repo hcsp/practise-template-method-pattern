d9e49ba29d9e90058e7f7d226217fda777e4980f
angus <tsang1205@qq.com>
2022-04-24 21:21:35 +0800

    fix: template method

--- src/main/java/com/github/hcsp/inheritance/MonsterStory.java
+++ src/main/java/com/github/hcsp/inheritance/MonsterStory.java
@@ -9,11 +9,16 @@ public class MonsterStory extends Story {
     // 你还想听吗
     public static void main(String[] args) {
         new MonsterStory().tellStory();
-        System.out.println("你还想听吗");
     }
 
     @Override
     public void story() {
         System.out.println("从前有个老妖怪");
+    }
+
+    @Override
+    public void endStory() {
+        super.endStory();
+        System.out.println("你还想听吗");
     }
 }
