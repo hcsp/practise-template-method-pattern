08ae085c21c678ba6722566f6e3b9b181a0be5a2
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-11-04 21:43:25 +0800

    extends&override

--- src/main/java/com/github/hcsp/inheritance/Story.java
+++ src/main/java/com/github/hcsp/inheritance/Story.java
@@ -7,19 +7,17 @@ public class Story {
         endStory();
     }
 
-    public void startStory() {
-        System.out.println("开始讲故事啦");
+    public void startStory() {System.out.println("开始讲故事啦");
     }
 
-    public void story() {
-        System.out.println("从前有个老和尚");
+    public void story() {System.out.println("从前有个老和尚");
     }
 
-    public void endStory() {
-        System.out.println("故事讲完啦");
+    public void endStory() {System.out.println("故事讲完啦");
     }
+
+
 
     public static void main(String[] args) {
-        new Story().tellStory();
-    }
+        new Story().tellStory();}
 }
